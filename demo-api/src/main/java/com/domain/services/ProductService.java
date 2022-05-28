a743b58aa08d42018dd8f94768000f65f08980ac
Made Manik Datu Yasa <manikyasa@gmail.com>
2022-05-28 09:34:58 +0700

    Initial commit

---
+++ demo-api/src/main/java/com/domain/services/ProductService.java
@@ -0,0 +1,47 @@
+package com.domain.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import com.domain.models.entities.Product;
+import com.domain.models.repos.ProductRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public Product save(Product product) {
+        return productRepo.save(product);
+    }
+
+    public Product findOne(Long id) {
+        Optional<Product> product = productRepo.findById(id);
+        if (product.isPresent()) {
+            return product.get();
+        } else {
+            return null;
+        }
+        // return productRepo.findById(id).get();
+    }
+
+    public Iterable<Product> findAll() {
+        return productRepo.findAll();
+    }
+
+    public void removeOne(Long id) {
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> findByName(String name) {
+        return productRepo.findByNameContains(name);
+    }
+
+}
